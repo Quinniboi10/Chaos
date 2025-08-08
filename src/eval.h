ce9bde2dd35f4edcda4399689b9c84460fffb28b
Quinniboi10 <jsmithwek@outlook.com>
2025-08-07 14:06:09 -1000

    Value network from Vine Bench: 1083197

--- src/eval.h
+++ src/eval.h
@@ -2,4 +2,16 @@
 
 #include "board.h"
 
+// ************ NETWORK CONFIG ************
+constexpr i16   QA             = 255;
+constexpr i16   QB             = 64;
+constexpr i16   EVAL_SCALE     = 400;
+constexpr usize HL_SIZE        = 32;
+
+constexpr int ReLU   = 0;
+constexpr int CReLU  = 1;
+constexpr int SCReLU = 2;
+
+constexpr int ACTIVATION = CReLU;
+
 i32 evaluate(const Board& board);
