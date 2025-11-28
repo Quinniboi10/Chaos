b64173b8334ef8920e687e7b3061e2426a3abd71
Quinn <69563433+Quinniboi10@users.noreply.github.com>
2025-11-28 16:18:41 -0600

    Double policy HL Bench: 9294986

--- src/policy.h
+++ src/policy.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "node.h"
-#include "board.h"
 #include "searcher.h"
 
 // ************ POLICY NETWORK CONFIG ************
 constexpr i16   Q_P       = 128;
-constexpr usize HL_SIZE_P = 1024;
+constexpr usize HL_SIZE_P = 2048;
 
 constexpr int ACTIVATION_P = CReLU;
 
