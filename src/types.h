5d7cb1bd7a50211e2692d461d5ef740e50cc5ec8
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-07-08 14:40:29 -0700

    Move numbers to seperate header

--- src/types.h
+++ src/types.h
@@ -11,6 +11,7 @@
 #include <bit>
 
 #include "assert.h"
+#include "numbers.h"
 
 #if defined(_MSC_VER)
     #define ASSUME(cond) __assume(cond)
@@ -27,25 +28,6 @@
                 *((int*) 0)(); \
         } while (0)
 #endif
-
-using u64 = uint64_t;
-using u32 = uint32_t;
-using u16 = uint16_t;
-using u8  = uint8_t;
-
-using i64 = int64_t;
-using i32 = int32_t;
-using i16 = int16_t;
-using i8  = int8_t;
-
-#ifdef _MSC_VER
-    #include <__msvc_int128.hpp>
-using u128 = std::_Unsigned128;
-#else
-using u128 = unsigned __int128;
-#endif
-
-using usize = size_t;
 
 using std::popcount;
 using std::string;
