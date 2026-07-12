6f84983239b7e8fbb6ee0be7912ab72657a51e5e
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2026-07-12 12:23:53 -0700

    Remove build warnings and errors Bench: 5514481

--- src/numbers.h
+++ src/numbers.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 
 using u64 = uint64_t;
@@ -12,4 +13,11 @@ using i32 = int32_t;
 using i16 = int16_t;
 using i8  = int8_t;
 
-using usize = unsigned;
+using usize = std::size_t;
+
+#ifdef _MSC_VER
+    #include <__msvc_int128.hpp>
+using u128 = std::_Unsigned128;
+#else
+using u128 = unsigned __int128;
+#endif
--- src/util.h
+++ src/util.h
@@ -454,35 +454,35 @@ inline void slowPrint(string text, const usize delay = 30, std::ostream& out = c
 }
 
 namespace cursor {
-    static void clearAll(std::ostream& out = cout) {
+    [[maybe_unused]] static void clearAll(std::ostream& out = cout) {
         out << "\033[2J\033[H";
     }
-    static void clear(std::ostream& out = cout) {
+    [[maybe_unused]] static void clear(std::ostream& out = cout) {
         out << "\033[2K\r";
     }
-    static void clearDown(std::ostream& out = cout) {
+    [[maybe_unused]] static void clearDown(std::ostream& out = cout) {
         out << "\x1b[J";
     }
-    static void home(std::ostream& out = cout) {
+    [[maybe_unused]] static void home(std::ostream& out = cout) {
         out << "\033[H";
     }
-    static void up(std::ostream& out = cout) {
+    [[maybe_unused]] static void up(std::ostream& out = cout) {
         out << "\033[A";
     }
-    static void down(std::ostream& out = cout) {
+    [[maybe_unused]] static void down(std::ostream& out = cout) {
         out << "\033[B";
     }
-    static void begin(std::ostream& out = cout) {
+    [[maybe_unused]] static void begin(std::ostream& out = cout) {
         out << "\033[1G";
     }
-    static void goTo(const usize x, const usize y, std::ostream& out = cout) {
+    [[maybe_unused]] static void goTo(const usize x, const usize y, std::ostream& out = cout) {
         out << "\033[" << y << ";" << x << "H";
     }
 
-    static void hide(std::ostream& out = cout) {
+    [[maybe_unused]] static void hide(std::ostream& out = cout) {
         out << "\033[?25l";
     }
-    static void show(std::ostream& out = cout) {
+    [[maybe_unused]] static void show(std::ostream& out = cout) {
         out << "\033[?25h";
     }
 }
